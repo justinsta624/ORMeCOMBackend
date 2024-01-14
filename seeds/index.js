d62a60ba34fe00003b0fe596dbb05b3836a0e8dd
justinsta624 <justinsta624@gmail.com>
2024-01-13 20:37:50 -0500

    update values in routes.js
    Fill out the unfinished routes in product-routes.js, tag-routes.js, and category-routes.js to perform create, read, update, and delete operations using Sequelize models.

--- seeds/index.js
+++ seeds/index.js
@@ -2,8 +2,8 @@ const seedCategories = require('./category-seeds');
 const seedProducts = require('./product-seeds');
 const seedTags = require('./tag-seeds');
 const seedProductTags = require('./product-tag-seeds');
-
 const sequelize = require('../config/connection');
+require('dotenv').config();
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
