cc3a77e64a5233d36f1378385acdfff589590596
justinsta624 <justinsta624@gmail.com>
2024-01-15 23:14:53 -0500

    Update server.js
    change the port & dotenv

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
